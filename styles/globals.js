8b7bdbb9088fb0bede2594cb36dbaa84b6e796ce
chimdie <nzechimdi@yahoo.com>
2021-07-09 21:52:32 +0100

    creating global theme

---
+++ styles/globals.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *,*::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body{
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a{
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
