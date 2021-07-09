6a890d3bb24286e91afa81e802c116e10be94a31
chimdie <nzechimdi@yahoo.com>
2021-07-09 21:52:03 +0100

    starting up shop

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import React from 'react'
+import Layout from '../components/Layout'
+import GlobalStyle from "../styles/globals";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Layout> 
+  );
 }
 
-export default MyApp
+export default MyApp;
