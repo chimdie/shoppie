6c6debead0ffce35b418362b3f7198e6f2e7781d
chimdie <nzechimdi@yahoo.com>
2021-07-14 15:03:09 +0100

    feat: layout components

---
+++ layout/layout.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0 2rem;
+  margin-bottom: 2rem;
+`;
+
+const Title = styled.h2`
+  font-size: 2rem;
+  text-transform: uppercase;
+  margin: 0 1rem 1rem;
+`;
+
+const Cards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+  gap: 2.5rem 1rem;
+  place-items: center;
+`;
+
+export { Container, Title, Cards };
