b36c2720ba4744af0edd1ca14a000812b6f18be8
chimdie <nzechimdi@yahoo.com>
2021-07-15 22:09:24 +0100

    changes

--- pages/category/[name].js
+++ pages/category/[name].js
@@ -4,17 +4,17 @@ import ProdCard from "../cards/ProdCard";
 import { Container, Cards, Title } from "../../layout/layout.style";
 
 const Category = ({ products }) => {
-    const router = useRouter();
-    return (
-        <Container>
-            <Title>Results for {router.query.category}</Title>
-            <Cards>
-                {products.map((product) => (
-                    <ProdCard key={product.id} product={product} />
-                ))}
-            </Cards>
-        </Container>
-    );
+  const router = useRouter();
+  return (
+    <Container>
+      <Title>Results for {router.query.category}</Title>
+      <Cards>
+        {products.map((product) => (
+          <ProdCard key={product.id} product={product} />
+        ))}
+      </Cards>
+    </Container>
+  );
 };
 
 export default Category;
@@ -25,27 +25,28 @@ export default Category;
 //     return { props: { products } };
 // }
 
-
 export async function getServerSideProps({ req, query }) {
-    // Fetch data from external API
-    // console.log("context : :", Object.keys(req));
-    let products = await fetch(`http://${req.headers.host}/api/products?q=${query.name}`)
-        .then((res) => {
-            if (!res.ok) {
-                throw res.statusText;
-            }
-            return res.json(); //we only get here if there is no error
-        })
-        .then(function (data) {
-            console.log({ data });
-            return data;
-        })
-        .catch((err) => {
-            console.log({ err });
-        });
+  // Fetch data from external API
+  // console.log("context : :", Object.keys(req));
+  let products = await fetch(
+    `http://${req.headers.host}/api/products?q=${query.name}`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw res.statusText;
+      }
+      return res.json(); //we only get here if there is no error
+    })
+    .then(function (data) {
+      console.log({ data });
+      return data;
+    })
+    .catch((err) => {
+      console.log({ err });
+    });
 
-    // Pass data to the page via props
-    return {
-        props: { products },
-    };
+  // Pass data to the page via props
+  return {
+    props: { products },
+  };
 }
