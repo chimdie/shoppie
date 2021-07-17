62beea358c3c657771844198db76cef67fa7e696
chimdie <nzechimdi@yahoo.com>
2021-07-17 16:13:17 +0100

    feat: api routes

--- pages/api/products/[category].js
+++ pages/api/products/[category].js
@@ -1,15 +1,13 @@
-// import data from "./data.json";
-const data = require('./data.json')
+const data = require('../data.json')
 
 export const getProdCat = (category) => {
   const products = data.filter((prod) => prod.category === category);
-  // console.log({ cat: req.query, products })
   return products;
 };
 
 export default function handler(req, res) {
   if (req.method !== "GET") {
-    res.setHeader("Allow", ["GET"]);
+    res.setHeader("Allow", ["GET"]);// Allow only GET request
     res.status(405).json({ message: `Method ${req.method} is not allowed` });
   } else {
     const products = getProdCat(req.query.category);
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,17 +1,15 @@
-const data = require("./data.json");
+const data = require("../data.json");
 
 export const getProducts = () => {
   return data;
 };
 
 export default function handler(req, res) {
+  const products = getProducts();
   if (req.method !== "GET") {
-    res.setHeader("Allow", ["GET"]);
+    res.setHeader("Allow", ["GET"]); // Allow only GET request
     res.status(405).json({ message: `Method ${req.method} not allowed` });
   } else {
-    const products = getProducts();
     res.status(200).json(products);
   }
 }
-
-
