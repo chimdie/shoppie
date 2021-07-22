5514bed7fa67f8410553b0e1d90f13a5c1137695
chimdie <nzechimdi@yahoo.com>
2021-07-22 23:14:17 +0100

    updates cart page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import store from "../redux/store";
+import {store} from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
