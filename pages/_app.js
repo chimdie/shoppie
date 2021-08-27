1b1d4f47094f3f08ed40d2f37bbfe9687352f1f5
chimdie <nzechimdi@yahoo.com>
2021-08-27 23:17:03 +0100

    fix: imported the navbar component to be visible to redux store

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
-import {store} from "../redux/store";
+import store from "../redux/store";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(
     <Provider store={store}>
+      <Navbar />
       <Component {...pageProps} />
     </Provider>
   );
